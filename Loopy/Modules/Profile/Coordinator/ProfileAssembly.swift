73cc68ea8c1c4cecadff2555847f20c51b734121
Vladislav <mashkovjob@yandex.ru>
2022-10-17 21:35:56 +0400

    created tabBar

--- Loopy/Modules/Profile/Coordinator/ProfileAssembly.swift
+++ Loopy/Modules/Profile/Coordinator/ProfileAssembly.swift
@@ -14,8 +14,18 @@ final class ProfileAssembly: BaseAssembly {
     func configure() -> UIViewController {
         let model = ProfileViewModel()
         let view = ProfileView.controllerFromStoryboard(storyboard)
+        
+        view.tabBarItem = ProfileAssembly.tabBarCustomItem
+        view.tabBarItem.imageInsets = .init(top: 12, left: 0, bottom: -12, right: 0)
         view.model = model
         return view
     }
+    
+    private static let tabBarImage = UIImage(named: "TB_home_inactive")?.withRenderingMode(.alwaysOriginal)
+    private static let tabBarImageSelected = UIImage(named: "TB_home_active")?.withRenderingMode(.alwaysOriginal)
+    
+    private static var tabBarCustomItem: UITabBarItem { UITabBarItem(title: nil,
+                                                                     image: ProfileAssembly.tabBarImage,
+                                                                     selectedImage: ProfileAssembly.tabBarImageSelected) }
     
 }

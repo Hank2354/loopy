32f361a4b6715e1bd9fe36e5fbbb7231802892a2
Vladislav <mashkovjob@yandex.ru>
2022-10-23 16:44:29 +0400

    new assets

--- Loopy/Resources/ColorsConfig.swift
+++ Loopy/Resources/ColorsConfig.swift
@@ -9,5 +9,19 @@ import UIKit
 
 public final class ColorConfig {
     
-    static let TabBarColor = UIColor(named: "TabBarColor")!
+    static let tabBarColor  = UIColor(named: "TabBarColor")!
+    
+    static let background   = UIColor(named: "Background")!
+    
+    static let accentOrange = UIColor(named: "AccentOrange")!
+    
+    static let lavender     = UIColor(named: "Lavender")!
+    
+    static let lightOrange  = UIColor(named: "LightOrange")!
+    
+    static let salat        = UIColor(named: "Salat")!
+    
+    static let waterBlue    = UIColor(named: "WaterBlue")!
 }
+
+
--- Loopy/Resources/ImageConfig.swift
+++ Loopy/Resources/ImageConfig.swift
@@ -10,15 +10,24 @@ import UIKit
 public final class ImageConfig {
     
     // MARK: - Tab Bar
-    static let homeTabBarImage               = UIImage(named: "TB_home_inactive")!.withRenderingMode(.alwaysOriginal)
-    static let homeTabBarImageSelected       = UIImage(named: "TB_home_active")!.withRenderingMode(.alwaysOriginal)
+    static let homeTabBarImage               = UIImage(named: "TB_home_inactive")!
+    static let homeTabBarImageSelected       = UIImage(named: "TB_home_active")!
     
-    static let receiptsTabBarImage           = UIImage(named: "TB_receipts_inactive")!.withRenderingMode(.alwaysOriginal)
-    static let receiptsTabBarImageSelected   = UIImage(named: "TB_receipts_active")!.withRenderingMode(.alwaysOriginal)
+    static let receiptsTabBarImage           = UIImage(named: "TB_receipts_inactive")!
+    static let receiptsTabBarImageSelected   = UIImage(named: "TB_receipts_active")!
     
-    static let fitnessTabBarImage            = UIImage(named: "TB_works_inactive")!.withRenderingMode(.alwaysOriginal)
-    static let fitnessTabBarImageSelected    = UIImage(named: "TB_works_active")!.withRenderingMode(.alwaysOriginal)
+    static let fitnessTabBarImage            = UIImage(named: "TB_works_inactive")!
+    static let fitnessTabBarImageSelected    = UIImage(named: "TB_works_active")!
     
-    static let statisticsTabBarImage         = UIImage(named: "TB_statistics_inactive")!.withRenderingMode(.alwaysOriginal)
-    static let statisticsTabBarImageSelected = UIImage(named: "TB_statistics_active")!.withRenderingMode(.alwaysOriginal)
+    static let statisticsTabBarImage         = UIImage(named: "TB_statistics_inactive")!
+    static let statisticsTabBarImageSelected = UIImage(named: "TB_statistics_active")!
+}
+
+public extension ImageConfig {
+    
+    // MARK: - DailyIcons
+    static let water = UIImage(named: "water")!
+    static let sleep = UIImage(named: "sleep")!
+    static let steps = UIImage(named: "steps")!
+    static let food  = UIImage(named: "food")!
 }

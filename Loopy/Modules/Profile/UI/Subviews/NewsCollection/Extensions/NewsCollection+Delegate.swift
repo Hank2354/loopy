19875fccd1d33fb273a0ee7a330f714e46aa67e8
Vladislav <mashkovjob@yandex.ru>
2022-10-31 02:52:57 +0400

    highlight effect for news cell

--- Loopy/Modules/Profile/UI/Subviews/NewsCollection/Extensions/NewsCollection+Delegate.swift
+++ Loopy/Modules/Profile/UI/Subviews/NewsCollection/Extensions/NewsCollection+Delegate.swift
@@ -10,6 +10,23 @@ import UIKit
 extension NewsCollection: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        UIView.animate(withDuration: 0.15) { cell?.transform = CGAffineTransform(scaleX: 0.95, y: 0.95) }
+        completion: { _ in UIView.animate(withDuration: 0.15) { cell?.transform = .identity } }
         newsDelegate?.didSelectNews(news[indexPath.item])
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        UIView.animate(withDuration: 0.15) {
+            cell?.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        UIView.animate(withDuration: 0.15) {
+            cell?.transform = .identity
+        }
     }
 }
